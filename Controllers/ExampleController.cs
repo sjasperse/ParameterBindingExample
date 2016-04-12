45d0db46a71687eee5f2583100556d1acc1bad39
sco24559 <scott.jasperse@spectrumhealth.org>
2016-04-12 12:29:29 -0400

    Initial commit

---
+++ Controllers/ExampleController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ParameterBindingExample.Controllers
+{
+    public class ExampleController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            return this.Json(new { uri = this.Request.RequestUri });
+        }
+    }
+}
---
+++ Controllers/ExampleControllerTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+
+namespace ParameterBindingExample.Controllers
+{
+    [TestFixture]
+    public class ExampleControllerTests
+    {
+        [Test]
+        public void ExampleController_Get_ReturnsJsonWithRequestUri()
+        {
+            var requestUri = "http://somethingsomething/api/example";
+
+            var controller = new ExampleController();
+            controller.Request = new System.Net.Http.HttpRequestMessage(System.Net.Http.HttpMethod.Get, requestUri);
+
+            var result = controller.Get();
+            var json = result
+                                .ExecuteAsync(new CancellationToken()).Result
+                                .Content.ReadAsStringAsync().Result;
+            var jsonObj = JsonConvert.DeserializeObject<JToken>(json);
+
+            Assert.AreEqual(requestUri, jsonObj.Value<string>("uri"));
+
+        }
+    }
+}
